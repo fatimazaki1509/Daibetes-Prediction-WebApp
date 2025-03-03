ad036b51a334529f17b57995587b25e1e9a9a455
Fatima Zaki <160648258+fatimazaki1509@users.noreply.github.com>
2025-03-04 00:04:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
-
 import numpy as np
 import pickle
 import streamlit as st
-
-#loading the saved model
-loaded_model = pickle.load(open("C:/Users/FATIMA/Downloads/Deploy ML Model/trained_model.sav", 'rb'))
-
-import pickle
 import os
 
 # Get the correct file path of the trained model
@@ -16,35 +10,27 @@ model_path = os.path.join(os.path.dirname(__file__), "trained_model.sav")
 with open(model_path, "rb") as file:
     loaded_model = pickle.load(file)
 
-
-#creating a function for Prediction
-
+# Creating a function for Prediction
 def diabetes_prediction(input_data):
-    
+    # Changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data, dtype=np.float64)
 
-    # changing the input_data to numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    # Reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
     prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
 
-    if (prediction[0] == 0):
-      return'The person is not diabetic'
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
     else:
-      return'The person is diabetic'
-      
-      
+        return 'The person is diabetic'
+
 def main():
-    
-    #giving a title
+    # Giving a title
     st.title('Diabetes Prediction Web App')
-    
-    #getting the input data from user
-    
-    Pregnencies = st.text_input('Number of Pregnencies')
+
+    # Getting the input data from user
+    Pregnancies = st.text_input('Number of Pregnancies')
     Glucose = st.text_input('Glucose Level')
     BloodPressure = st.text_input('Blood Pressure Value')
     SkinThickness = st.text_input('Skin Thickness Value')
@@ -52,19 +38,24 @@ def main():
     BMI = st.text_input('BMI Value')
     DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function')
     Age = st.text_input('Age of the Person')
-    
-    #code for Prediction 
-    diagnosis = ''
-    
-    #creating a button for Prediction 
-    if st.button('Diabetes Test Result'):
-        diagnosis = diabetes_prediction([Pregnencies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
-        
-        
-    st.success(diagnosis)
-     
-     
-     
-if __name__ == '__main__':
 
-   main()     
+    # Code for Prediction
+    diagnosis = ''
+
+    # Creating a button for Prediction
+    if st.button('Diabetes Test Result'):
+        try:
+            # Convert inputs to float
+            input_data = [
+                float(Pregnancies), float(Glucose), float(BloodPressure), float(SkinThickness),
+                float(Insulin), float(BMI), float(DiabetesPedigreeFunction), float(Age)
+            ]
+            diagnosis = diabetes_prediction(input_data)
+        except ValueError:
+            diagnosis = "Please enter valid numeric values."
+
+    st.success(diagnosis)
+
+if __name__ == '__main__':
+    main()
+   
